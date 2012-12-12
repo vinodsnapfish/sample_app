87247d8e8f99686ebc7caf0a601d362ccf6fd506
Vinod <vinod@vinod-home.(none)>
2012-12-12 12:58:28 -0800

    Initial check in for sample_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '577c33fb7744e9489aa974c136899677fdd1fda25d5d7642870ab8d0f8d4876e98f9714c20e83bd63ec5894f5a8c86da8787d74068ca023fa152655383bd12ba'
